d482aa838ed50fa6715f4ed257cc2e4dba74773b
hwangyunju <hwangyunju@github.com>
2017-05-20 17:22:20 +0900

    update score.py(send score to db)

--- score.py
+++ score.py
@@ -1,5 +1,9 @@
-import MySQLdb, os, unittest
+import MySQLdb, os, sys
 from midi_comparator import MIDIComparator
+
+user_id = sys.argv[1]
+song_name = sys.argv[2]
+
 
 # connect db
 db = MySQLdb.connect(host="35.161.154.86",
@@ -11,7 +15,7 @@ db = MySQLdb.connect(host="35.161.154.86",
 cur = db.cursor()
 
 # folder stored user's midi file
-path_dir='/home/yunju/midi3/midi_compare'
+path_dir='tests/data'
 
 file_list=os.listdir(path_dir)
 file_list.sort()
@@ -22,32 +26,30 @@ for item in file_list:
 		print item
 		user_mid=item
 
-
-class TestPitch(unittest.TestCase):          
-	def test_one_note_diff_sequences(self):
-		mc = MIDIComparator(user_mid, 'tests/data/score3.mid')
-		# score3 is missing one note present in score 2
-		ratio = 100 * mc.get_similarity_ratio()
-		print 'score: %.f' % ratio 
-		#assert 0.87 < ratio < 0.92
+          
+def test_one_note_diff_sequences():
+	mc = MIDIComparator(user_mid, 'tests/data/score3.mid')
+	# score3 is missing one note present in score 2
+	ratio = 100 * mc.get_similarity_ratio()
+	print 'score: %.f' % ratio 
+	return ratio
+	#assert 0.87 < ratio < 0.92
                 
+ratio = test_one_note_diff_sequences()
 
 #--record score
-#sql = "INSERT INTO SCHOOL(user,score) VALUE(%d, %d)"
-#cur.execute("INSERT INTO SCHOOL VALUES(a,ratio)")
+insert_sql = "INSERT INTO SCORE(user,song,score) VALUE(%s, %s, %s)"
+cur.execute(insert_sql, (user_id, song_name, ratio))
 
-cur.execute("SELECT * FROM SCHOOL")
+cur.execute("SELECT * FROM SCORE")
 
 for row in cur.fetchall():
-	print row[0], row[1]
+	print row[0], row[1], row[2]
 
 db.commit()
 db.close()
 
 
-
-if __name__ == "__main__":
-    unittest.main()
 
 
 
